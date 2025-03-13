a5e5cd7c7520e7056cad9ecc70cec8134474304b
Haidar <afiq.haidar12@gmail.com>
2025-03-13 14:02:00 +0700

    feat: clerk sign in/up

---
+++ color.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                "reoprime": "#FFFFFF",
+                "reosec": "#7E8CA0",
+                "accent": "#1D4ED8"
+            }
+        }
+    }
+};
+      
